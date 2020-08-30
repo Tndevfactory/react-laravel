0c1d8f038b1d510fec0ad6c415513e396046f985
tndevfactory <tndevfactory@gmail.com>
2020-08-30 21:23:36 +0100

    first commit

---
+++ resources/js/Appres.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Example from './components/Example';
+import Ext from './components/Ext';
+
+
+function Appres() {
+  return (
+    <div className="App">
+     i am res
+     <Example />
+     <Ext />
+    </div>
+  );
+}
+
+export default Appres;
---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+require('./bootstrap');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Appres from './Appres';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Appres />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
